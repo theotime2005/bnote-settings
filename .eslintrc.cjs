01ce4b3fe244eaa79ca4bb47c0f7df423767ee29
theotime2005 <theotime.berthod@epitech.digital>
2024-07-23 14:31:34 +0200

    template to start

---
+++ .eslintrc.cjs
@@ -0,0 +1,22 @@
+/* eslint-env node */
+require("@rushstack/eslint-patch/modern-module-resolution");
+
+module.exports = {
+  root: true,
+  "extends": [
+    "plugin:vue/vue3-essential",
+    "eslint:recommended",
+    "@vue/eslint-config-prettier/skip-formatting",
+  ],
+  "rules": {
+    "semi": ["error", "always"],
+    "quotes": ["error", "double"],
+    "indent": ["error", 2],
+    "comma-dangle": ["error", "always-multiline"],
+    "comma-spacing": ["error", { "before": false, "after": true }],
+    "object-curly-spacing": ["error", "always"],
+  },
+  parserOptions: {
+    ecmaVersion: "latest",
+  },
+};
---
+++ src/views/Home.vue
@@ -0,0 +1,12 @@
+<script setup>
+
+</script>
+
+<template>
+  <h1>Accueil</h1>
+  <p>Bienvenu sur l'interface en ligne de gestion des paramètres de B.note.</p>
+</template>
+
+<style scoped>
+
+</style>
