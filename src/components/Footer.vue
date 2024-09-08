365d6beb53305bb1e21260ba76b7928652c9a933
theotime2005 <theotime.berthod@epitech.digital>
2024-09-08 23:29:17 +0200

    feat: Add a footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+  name: "Footer",
+};
+</script>
+
+<template>
+  <footer>
+    <p>Cette application web est libre de droit. Le code source est entièrement réutilisable et modifiable</p>
+    <a href="https://github.com/theotime2005/bnote-settings">Voir le code sur GitHub</a>
+    <p>Vos contribution sont les bienvenues.</p>
+  </footer>
+</template>
+
+<style scoped>
+
+</style>
