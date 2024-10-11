38dc23096d83818df7bb076a46a6a695e12108e0
theotime2005 <theotime.berthod@epitech.digital>
2024-10-11 10:55:15 +0200

    fix: Adjust download component

--- src/components/DownloadDesktopComponent.vue
+++ src/components/DownloadDesktopComponent.vue
@@ -34,35 +34,25 @@ export default {
     <h2 class="text-2xl font-semibold mb-4">{{ $t('desktop.title') }}</h2>
     <p class="text-lg mb-6">{{ $t('desktop.message') }}</p>
 
-    <!-- Order Mac/arm, Mac:x, Windows -->
+    <!-- Order Mac, Windows -->
     <article class="space-y-6">
       <div v-if="versionInformation" class="border border-gray-200 p-4 rounded-lg">
-        <h3 class="text-xl font-medium mb-2">{{ $t('desktop.macArm64') }}</h3>
+        <h3 class="text-xl font-medium mb-2">{{ $t('desktop.mac') }}</h3>
         <a
             :href="versionInformation.assets[0].browser_download_url"
             class="text-blue-600 hover:underline"
         >
-          {{ $t("desktop.downloadMacArm64", { version: versionInformation.tag_name }) }}
+          {{ $t("desktop.downloadMac", { version: versionInformation.tag_name }) }}
         </a>
       </div>
 
       <div v-if="versionInformation" class="border border-gray-200 p-4 rounded-lg">
-        <h3 class="text-xl font-medium mb-2">{{ $t('desktop.macX64') }}</h3>
+        <h3 class="text-xl font-medium mb-2">{{ $t('desktop.windows') }}</h3>
         <a
             :href="versionInformation.assets[1].browser_download_url"
             class="text-blue-600 hover:underline"
         >
-          {{ $t("desktop.downloadMacX64", { version: versionInformation.tag_name }) }}
-        </a>
-      </div>
-
-      <div v-if="versionInformation" class="border border-gray-200 p-4 rounded-lg">
-        <h3 class="text-xl font-medium mb-2">{{ $t('desktop.windowsX64') }}</h3>
-        <a
-            :href="versionInformation.assets[2].browser_download_url"
-            class="text-blue-600 hover:underline"
-        >
-          {{ $t("desktop.downloadWindowsX64", { version: versionInformation.tag_name }) }}
+          {{ $t("desktop.downloadWindows", { version: versionInformation.tag_name }) }}
         </a>
       </div>
 
