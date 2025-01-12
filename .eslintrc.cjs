6925efc8cbf2db6025ff17604eaf6b71ec1e35bf
theotime2005 <theotime.berthod@epitech.digital>
2025-01-12 22:20:14 +0100

    fix: Adjust eslint for tests

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,21 +2,32 @@
 require("@rushstack/eslint-patch/modern-module-resolution");
 
 module.exports = {
+  root: true,
   env: {
     node: true,
   },
-  root: true,
-  "extends": [
+  extends: [
     "plugin:vue/vue3-essential",
     "eslint:recommended",
     "@vue/eslint-config-prettier/skip-formatting",
   ],
-  "rules": {
-    "semi": ["error", "always"],
-    "quotes": ["error", "double"],
-    "indent": ["error", 2],
+  plugins: [
+    "vitest-globals",
+  ],
+  overrides: [
+    {
+      files: ["**/__tests__/*.{j,t}s?(x)", "**/*.spec.{j,t}s?(x)"],
+      env: {
+        "vitest-globals/env": true,
+      },
+    },
+  ],
+  rules: {
+    semi: ["error", "always"],
+    quotes: ["error", "double"],
+    indent: ["error", 2],
     "comma-dangle": ["error", "always-multiline"],
-    "comma-spacing": ["error", { "before": false, "after": true }],
+    "comma-spacing": ["error", { before: false, after: true }],
     "object-curly-spacing": ["error", "always"],
   },
   parserOptions: {
