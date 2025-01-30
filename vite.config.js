3e73dad23eec9adf1a86db0a6efc5e2768ea6b3f
theotime2005 <theotime.berthod@epitech.digital>
2025-01-30 22:48:26 +0100

    feat: Configure to use env variables

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,25 @@
 /* eslint-disable */
-import vue from "@vitejs/plugin-vue";
 import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from "vite";
-// https://vitejs.dev/config/
+import vue from "@vitejs/plugin-vue";
+import { dirname } from "path";
+import { defineConfig, loadEnv } from "vite";
+import vitePluginEnvCompatible from "vite-plugin-env-compatible";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const env = loadEnv(process.env.MODE || "development", __dirname);
+
 export default defineConfig({
   plugins: [
     vue(),
+    vitePluginEnvCompatible(),
   ],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
+  },
+  define: {
+    __APP_ENV__: JSON.stringify(env.APP_ENV),
   },
 });
