b7c5bb3b016b61261faf0693af64589a92e6d2cc
theotime2005 <theotime.berthod@epitech.digital>
2025-02-01 10:31:56 +0100

    Revert "2.4.0"
    This reverts commit 5899b1ef04094e840051691d724dd49b04c9a290.

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -13,24 +13,19 @@ export default {
 };
 </script>
 <template>
-  <footer
-    class="bg-slate-900 p-2 flex flex-col items-center justify-center text-[11px] border-t border-t-slate-600"
-    :aria-label="$t('footer.label')"
-  >
+  <footer class="bg-slate-900 p-2 flex flex-col items-center justify-center text-[11px] border-t border-t-slate-600" :aria-label="$t('footer.label')">
     <div class="flex">
       <div class="flex flex-col justify-center items-center">
-        <p>{{ $t("footer.message1") }}</p>
-        <p>{{ $t("footer.message2") }}</p>
+        <p>{{$t('footer.message1')}}</p>
+        <p>{{$t('footer.message2')}}</p>
       </div>
     </div>
-    <LanguageComponent />
-    <a
-      class="text-green-400 hover:text-green-300 hover:underline transition-all duration-200"
-      href="https://github.com/theotime2005/bnote-settings"
-      >{{ $t("footer.code") }}</a
-    >
-    <p>{{ $t("footer.version", { version: version }) }}</p>
+    <LanguageComponent/>
+    <a class="text-green-400 hover:text-green-300 hover:underline transition-all duration-200" href="https://github.com/theotime2005/bnote-settings">{{$t('footer.code')}}</a>
+    <p>{{$t('footer.version', {version: version})}}</p>
   </footer>
 </template>
 
-<style scoped></style>
+<style scoped>
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,21 +6,19 @@ export default {
 
 <template>
   <div class="flex flex-col p-3">
-    <h1 class="text-3xl mb-3">{{ $t("home.title") }}</h1>
+    <h1 class="text-3xl mb-3">{{$t('home.title')}}</h1>
 
     <div class="flex flex-col text-justify">
-      <p>{{ $t("home.message-1") }}</p>
-      <RouterLink
-        class="text-left w-fit flex items-center justify-center text-green-400 rounded duration-200 transition-all hover:border-green-400 hover:text-green-200 hover:underline"
-        to="/settings"
-        >{{ $t("home.goto-settings") }}</RouterLink
-      >
+      <p>{{$t('home.message-1')}}</p>
+      <RouterLink class="text-left w-fit flex items-center justify-center text-green-400 rounded duration-200 transition-all hover:border-green-400 hover:text-green-200 hover:underline" to="/settings">{{$t('home.goto-settings')}}</RouterLink>
     </div>
     <div class="flex flex-col text-justify">
-      <h2 class="text-2xl mt-5">{{ $t("home.title2") }}</h2>
-      <p>{{ $t("home.message2") }}</p>
+      <h2 class="text-2xl mt-5">{{$t('home.title2')}}</h2>
+      <p>{{$t('home.message2')}}</p>
     </div>
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+
+</style>
