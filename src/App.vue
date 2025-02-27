caa95259436fcda3212f47a4bf0486c16a9bd761
theotime2005 <theotime.berthod@epitech.digital>
2025-02-27 23:14:14 +0100

    feat: convert app to legacy api

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,40 @@
-<script setup>
-import { onBeforeMount, ref } from "vue";
-import { useI18n } from "vue-i18n";
-import { RouterView } from "vue-router";
-
+<script>
 import FooterComponent from "@/components/FooterComponent.vue";
 import NavBarComponent from "@/components/NavBarComponent.vue";
 import { useLocaleCookie } from "@/scripts/useLocaleCookie.js";
 
-const mainRef = ref(null);
+export default {
+  components: {
+    FooterComponent,
+    NavBarComponent,
+  },
+  data() {
+    return {
+      hasLanguage: false,
+      canReset: false,
+    };
+  },
 
-const focusMain = () => {
-  if (mainRef.value) {
-    mainRef.value.focus();
-  }
+  methods: {
+    focusMain() {
+      if (this.$refs.mainRef) {
+        this.$refs.mainRef.focus();
+      }
+    },
+  },
+
+  beforeMount() {
+    // set locale from cookie
+    const localeCookie = useLocaleCookie.getLocaleCookie();
+    if (localeCookie) {
+      this.$i18n.locale = localeCookie;
+    }
+  },
 };
-
-onBeforeMount(function() {
-  // set locale from cookie
-  const localeCookie = useLocaleCookie.getLocaleCookie();
-  if (localeCookie) {
-    useI18n().locale.value = localeCookie;
-  }
-});
 </script>
 
 <template>
-  <button @click="focusMain" class="sr-only">{{$t('skip-content')}}</button>
+  <button @click="focusMain" class="sr-only">{{ $t("skip-content") }}</button>
   <NavBarComponent @move-cursor="focusMain" />
   <main class="flex flex-col grow overflow-y-scroll" ref="mainRef" tabindex="-1">
     <RouterView />
