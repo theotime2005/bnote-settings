0e107396626c7a242c03a44f3b96a858774f11a3
theotime2005 <theotime.berthod@epitech.digital>
2025-03-02 13:40:35 +0100

    feat: remove unused plugin for eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,21 +1,18 @@
-/* eslint-disable */
+import path from "node:path";
 import { fileURLToPath } from "node:url";
 
-import { fixupPluginRules } from "@eslint/compat";
 import { FlatCompat } from "@eslint/eslintrc";
 import js from "@eslint/js";
-import _import from "eslint-plugin-import";
 import simpleImportSort from "eslint-plugin-simple-import-sort";
 import vitestGlobals from "eslint-plugin-vitest-globals";
 import globals from "globals";
-import path from "node:path";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({
   baseDirectory: __dirname,
   recommendedConfig: js.configs.recommended,
-  allConfig: js.configs.all
+  allConfig: js.configs.all,
 });
 
 export default [
@@ -23,22 +20,21 @@ export default [
     "plugin:vue/vue3-essential",
     "eslint:recommended",
     "@vue/eslint-config-prettier/skip-formatting",
-    "plugin:i18n-json/recommended"
+    "plugin:i18n-json/recommended",
   ),
   {
     plugins: {
-      import: fixupPluginRules(_import),
       "simple-import-sort": simpleImportSort,
-      "vitest-globals": vitestGlobals
+      "vitest-globals": vitestGlobals,
     },
 
     languageOptions: {
       globals: {
-        ...globals.node
+        ...globals.node,
       },
 
       ecmaVersion: "latest",
-      sourceType: "module"
+      sourceType: "module",
     },
 
     rules: {
@@ -51,36 +47,23 @@ export default [
         "error",
         {
           before: false,
-          after: true
-        }
+          after: true,
+        },
       ],
 
       "object-curly-spacing": ["error", "always"],
       "simple-import-sort/imports": "error",
       "simple-import-sort/exports": "error",
-
-      "import/order": [
-        "error",
-        {
-          groups: [["builtin", "external"], ["internal"], ["parent", "sibling"], ["index"]],
-          "newlines-between": "always",
-
-          alphabetize: {
-            order: "asc",
-            caseInsensitive: true
-          }
-        }
-      ]
-    }
+    },
   },
   {
     files: ["**/__tests__/*.{j,t}s?(x)", "**/*.spec.{j,t}s?(x)"],
 
     languageOptions: {
       globals: {
-        ...vitestGlobals.environments.env.globals
-      }
-    }
+        ...vitestGlobals.environments.env.globals,
+      },
+    },
   },
   {
     ignores: [
@@ -90,7 +73,7 @@ export default [
       ".prettierrc.json",
       "jsconfig.json",
       "package-lock.json",
-      "package.json"
-    ]
-  }
+      "package.json",
+    ],
+  },
 ];
