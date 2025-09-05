c6aa21a9bca650262cdc6c05e90faf0aa527e1ea
theotime2005 <theotime.berthod@epitech.digital>
2025-09-05 14:40:53 +0200

    feat(i18n): put title only if locale is defined

--- src/router/index.js
+++ src/router/index.js
@@ -1,18 +1,15 @@
-
 import { createRouter, createWebHashHistory } from "vue-router";
 
 import i18n from "@/i18n.js";
 import routesList from "@/router/router-list.js";
-import { useLocaleCookie } from "@/scripts/useLocaleCookie.js";
-
 
 const router = createRouter({
   history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: routesList,
 });
-const t = i18n.global.t;
+const { t, locale } = i18n.global;
 router.afterEach((to) => {
-  if (useLocaleCookie.getLocaleCookie()) {
+  if (locale.value) {
     document.title = `${t(`${to.name}.title`)} | ${t("title")}`;
   } else {
     document.title = "B.note";
